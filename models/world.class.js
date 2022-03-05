c22c655074b9df3178a56afae5c3497e7a8cdab5
navjotsingh96 <navjot.singh96@web.de>
2022-03-05 13:52:17 +0100

    Added bacjground when Map on the end

--- models/world.class.js
+++ models/world.class.js
@@ -10,10 +10,15 @@ class World {
         new Clouds()
     ];
     backgroundobjects = [
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719 * 20),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', -719 * 20),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', -719 * 20),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', -719 * 20),
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719 * 3),
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -719 * 3),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -719 * 3),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -719 * 3),
+
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719 * 2),
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', -719 * 2),
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', -719 * 2),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', -719 * 2),
 
         new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719),
         new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -719),
