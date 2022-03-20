f4094c5fbdda8b87066c2dbce0a0e067d6a2a8fb
navjotsingh96 <navjot.singh96@web.de>
2022-03-20 13:53:25 +0100

    added new level calss

---
+++ models/level.class.js
@@ -0,0 +1,10 @@
+class Level {
+    enemies;
+    clouds;
+    backgroundobjects;
+    constructor(enemies, clouds, backgroundobjects) {
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.backgroundobjects = backgroundobjects;
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,55 +1,9 @@
 class World {
 
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Clouds()
-    ];
-    backgroundobjects = [
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -719 * 3),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', -719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', -719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', -719 * 2),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -719),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -719),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 719 * 2),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 719 * 2),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719 * 3),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719 * 3),
-
-        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 4),
-        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 719 * 4),
-        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/1.png', 719 * 4),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 719 * 4),
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundobjects = level1.backgroundobjects;
     canvas;
     ctx;
     keyboard;
